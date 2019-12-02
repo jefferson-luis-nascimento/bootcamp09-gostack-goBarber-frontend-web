4f5e4fea5cf34b0a5814dbcc0939890e5d883894
jefferson-luis-nascimento <je.luis.nascimento@hotmail.com>
2019-12-02 19:19:49 -0200

    Atualizando perfil

--- src/store/modules/user/sagas.js
+++ src/store/modules/user/sagas.js
@@ -1,3 +1,24 @@
-import { all } from 'redux-saga/effects';
+import { takeLatest, put, call, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
-export default all([]);
+import api from '~/services/api';
+import { updateProfileSuccess, updateProfileFailiure } from './actions';
+
+export function* updateProfile({ payload }) {
+  try {
+    const { name, email, ...rest } = payload.data;
+
+    const profile = { name, email, ...(rest.oldPassword ? rest : {}) };
+
+    const response = yield call(api.put, 'users', profile);
+
+    toast.success('Perfil atualizado com sucesso.');
+
+    yield put(updateProfileSuccess(response.data));
+  } catch (error) {
+    toast.error('Erro ao atualizar o perfil. Confira seus dados');
+    yield put(updateProfileFailiure());
+  }
+}
+
+export default all([takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)]);
