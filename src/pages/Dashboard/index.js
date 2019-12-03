9d3c4a0f0ab8581167659ab0d8ea77442e18f99c
jefferson-luis-nascimento <je.luis.nascimento@hotmail.com>
2019-12-03 19:47:11 -0200

    Estilização do Dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
 import api from '~/services/api';
-
-// import { Container } from './styles';
+import { Container, Time } from './styles';
 
 export default function Dashboard() {
-  api.get('/appointments');
-  return <h1>Dashboard</h1>;
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft color="#fff" size={36} />
+        </button>
+        <strong>31 de Maio</strong>
+        <button type="button">
+          <MdChevronRight color="#fff" size={36} />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Jefferson Luís Nascimento</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>Em Aberto</span>
+        </Time>
+        <Time>
+          <strong>10:00</strong>
+          <span>Em Aberto</span>
+        </Time>
+        <Time>
+          <strong>11:00</strong>
+          <span>Em Aberto</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
